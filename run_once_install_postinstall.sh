02e863eef911b81a389717d8b22f6e53febe0736
mmcfetridge1969 <91107715+mmcfetridge1969@users.noreply.github.com>
2025-07-14 08:02:41 -0400

    update

--- run_once_install_postinstall.sh
+++ run_once_install_postinstall.sh
@@ -1,13 +1,5 @@
 -
 #!/bin/sh
-# Define and Add 1920x1080 screen Resolution
-# cvt 1920 1080
-xrandr --newmode "1920x1080_60.00"  173.00  1920 2048 2248 2576  1080 1083 1088 1120 -hsync +vsync
-xrandr --newmode "1600x900_60.00"  118.25  1600 1696 1856 2112  900 903 908 934 -hsync +vsync
-xrandr --addmode eDP-1 1920x1080_60.00
-xrandr --addmode eDP-1 1600x900_60.00
-#xrandr --output eDP-1 --mode 1600x900_60.00
-xrandr --output eDP-1 --mode 1920x1080_60.00
 
 # Install Airvpn Client (eddie)
 curl -fsSL https://eddie.website/repository/keys/eddie_maintainer_gpg.key | sudo tee /usr/share/keyrings/eddie.website-keyring.asc > /dev/null
